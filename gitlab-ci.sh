62a009e17e88a9403ab57c0b80eb594c7e89c3a0
Emanuele Falzone <emafalzo@gmail.com>
2019-10-27 11:36:26 +0100

    Run all jobs and set all the environment variables specified in the settings of the repository

---
+++ gitlab-ci.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+set -e
+
+################################################################################
+# Check if dependencies are installed
+################################################################################
+
+function check_dependecy {
+    if ! [ -x $(command -v $1) ]; then
+        echo "Error: $1 is not installed."
+        exit 1
+    fi
+}
+
+check_dependecy git
+check_dependecy curl
+check_dependecy jq
+check_dependecy yq
+check_dependecy gitlab-runner
+
+
+################################################################################
+# Check if we are in a git repository
+################################################################################
+
+if ! [ -d .git ]; then
+    echo 'Not in a git repository!'
+    exit 1
+fi
+
+
+################################################################################
+# Check if there is a .gitlab-ci.yml file
+################################################################################
+
+if ! [ -f .gitlab-ci.yml ]; then
+    echo 'Missing .gitlab-ci.yml file!'
+    exit 1
+fi
+
+
+################################################################################
+# Check if the required environment variables have been set
+################################################################################
+
+function check_variable {
+    if ! [ -n $1 ]; then
+        echo "Error: $1 variable is not set."
+        exit 1
+    fi
+}
+
+check_variable GITLAB_CI_TOKEN
+
+
+################################################################################
+# Retrieve CI/CD environment variables from gitlab
+################################################################################
+
+SSH_URL_TO_REPO=$(git remote get-url origin)
+
+HTTP_URL_TO_REPO=$(echo $SSH_URL_TO_REPO \
+                        | sed 's/:.*//' \
+                        | sed 's/git@/https:\/\//')
+
+PROJECT_NAME=$(echo $SSH_URL_TO_REPO \
+                    | sed 's/.*://' \
+                    | sed 's/.git//' \
+                    | sed 's/\//%2F/')
+
+VARIABLES_LINK=$(curl --insecure \
+                      -H "Private-Token: ${GITLAB_CI_TOKEN}" \
+                      $HTTP_URL_TO_REPO/api/v4/projects/$PROJECT_NAME \
+                | jq --raw-output '._links.self + "/variables"')
+
+PROJECT_VARIABLES_LINK="${PROJECT_LINK}/variables"
+
+PROJECT_VARIABLES=$(curl --insecure \
+                         -H "Private-Token: ${GITLAB_CI_TOKEN}" \
+                         ${VARIABLES_LINK} \
+                   | jq --raw-output 'map("--env " + .key + "=" + .value) 
+                                     | join(" ")')
+
+################################################################################
+# Extract list of jobs from .gitlab-ci.yml file
+################################################################################
+
+JOBS=($(yq r -j .gitlab-ci.yml \
+        | jq -r 'with_entries(select(.value | objects)) 
+                | with_entries(select(.value.stage | strings)) 
+                | keys 
+                | @sh' \
+        | sed "s/\'//g"))
+
+################################################################################
+# If job name specified executes it, otherwise execute all jobs
+################################################################################
+
+for JOB in "${JOBS[@]}"
+do
+    gitlab-runner exec docker $JOB \
+        --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
+        --env CI_COMMIT_SHA=$(git rev-parse HEAD) \
+        $PROJECT_VARIABLES
+done
